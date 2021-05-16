0f3087af27e9cbe2d8a1ac659c77171c2916ac43
slain57 <84188269+slain57@users.noreply.github.com>
2021-05-16 12:27:42 +0300

    Update project_botSN.py

--- project_botSN.py
+++ project_botSN.py
@@ -16,7 +16,7 @@ bot = telebot.TeleBot(api_telegram)
 def start_message(message):
     keyboard1 = telebot.types.ReplyKeyboardMarkup()
     keyboard1.row('Погода', 'Курс валют')
-    keyboard1.row('Новости', 'Задачи')
+    keyboard1.row('Новости')
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
     keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
 
@@ -31,12 +31,11 @@ def welcome(message):
 def test(message):
     if message.text == "Погода":
         bot.send_message(message.chat.id, 'Добро пожаловать, ' + str(message.from_user.first_name) + ',' + '\n' +
-                         'чтоб узнать погоду напишите в чат название города')
+                         'чтоб узнать погоду напишите в чат название города,но в начале поставьте точку (Пример: .Москва)')
 
     if message.text == "Курс валют":
         a = requests.get(url3)
         a = a.json()
-        print(a)
         for key, value in a.items():
             print(key, ":", value)
         bot.send_message(message.chat.id,"Доллар 1$ = " + str(a['Valute']['USD']['Value']) + "₽" )
@@ -50,16 +49,15 @@ def test(message):
     if message.text == "Новости":
         b= requests.get(url_novosti)
         b = b.json()
-        print(b)
         for key, value in b.items():
-            print(key, ":", value)
+            key, ":", value
         for i in range(10):
             bot.send_message(message.chat.id,"Источник: " + b["articles"][i]['source']["name"])
             bot.send_message(message.chat.id, b["articles"][i]["title"])
             bot.send_message(message.chat.id, "Ссылка на новость: " + b["articles"][i]["url"])
     if message.text == "Задачи":
         pass
-    if message.text.startswith("!"):
+    if message.text.startswith("."):
         city_name = message.text[1:]
         try:
             params = {'APPID': api_weather, 'q': city_name, 'units': 'metric', 'lang': 'ru'}
@@ -69,24 +67,24 @@ def test(message):
 
             if weather["main"]['temp'] < 0:
                 status = "Сейчас холодно!"
-            elif weather["main"]['temp'] < 10:
-                status = "Сейчас прохолодно!"
-            elif weather["main"]['temp'] < 20:
+            elif weather["main"]['temp'] <= 10:
+                status = "Сейчас прохладно!"
+            elif weather["main"]['temp'] <= 20:
                 status = "Сейчас тепло!"
-            elif weather["main"]['temp'] > 38:
+            elif weather["main"]['temp'] >= 25:
                 status = "Сейчас жарко!"
             else:
-                status = "Сейчас отличная температура!"
+                status = "Сейчас отличная погода!"
 
             bot.send_message(message.chat.id, "В городе " + str(weather["name"]) + " температура " + str(
                 float(weather["main"]['temp'])) + "\n" +
                              "Максимальная температура " + str(float(weather['main']['temp_max'])) + "\n" +
-                             "Минимальная температура " + str(float(weather['main']['temp_min'])) + "\n" +
-                             "Скорость ветра " + str(float(weather['wind']['speed'])) + "\n" +
-                             "Давление " + str(float(weather['main']['pressure'])) + "\n" +
-                             "Влажность " + str(int(weather['main']['humidity'])) + "%" + "\n" +
-                             "Видимость " + str(weather['visibility']) + "\n" +
-                             "Описание " + str(weather['weather'][0]["description"]) + "\n\n" + status)
+                             "Минимальная температура  " + str(float(weather['main']['temp_min'])) + "\n" +
+                             "Скорость ветра - " + str(float(weather['wind']['speed'])) + "\n" +
+                             "Давление - " + str(float(weather['main']['pressure'])) + "\n" +
+                             "Влажность - " + str(int(weather['main']['humidity'])) + "%" + "\n" +
+                             "Видимость - " + str(weather['visibility']) + "\n" +
+                             "Описание: " + str(weather['weather'][0]["description"]) + "\n\n" + status)
 
         except:
             bot.send_message(message.chat.id, "Город " + city_name + " не найден")
